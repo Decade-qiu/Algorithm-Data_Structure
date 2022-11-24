e7d211db2ba5c8f55243c31041f89108527b99b9
Decade-qiu <3237550600@qq.com>
2022-11-24 21:29:20 +0800

    2022/11/24

--- 日常/A/Main.java
+++ 日常/A/Main.java
@@ -1,31 +1,27 @@
 import java.util.*;
 
 import java.io.*;
+import java.lang.reflect.Array;
 
 public class Main {
     static String ss, io[];
-    static int test, N = 100010, M = 10000007;
-    static int n, m;
-    static int a[] = new int[N], tr[] = new int[N], ans[] = new int[N];
+    static int test, N = 10010, M = 10000007;
+    static int n, m, k;
+    static int a[] = new int[N];
     static void solve() throws IOException{
         n = ni();
-        for (int i = 0;i < n;i++) {
-            a[i] = ni();
-        }
-        tr[n-1] = a[n-1];
-        for (int i = n-2;i >= 0;i--) tr[i] = min(tr[i+1], a[i]);
+        int z = -1, o = -1, ans = 0;
         for (int i = 0;i < n;i++){
-            int v = a[i];
-            int l = i, r = n-1;
-            while (l <= r){
-                int m = l+r >> 1;
-                if (tr[m] >= v) r = m-1;
-                else l = m+1;
+            a[i] = ni();
+            if (a[i] == 0){
+                ans += (i-o) + (o+1)*2;
+                z = i;
+            }else{
+                ans += (z+1)*2;
+                o = i;
             }
-            if (l == i) ans[i] = -1;
-            else ans[i] = l-i-2;
         }
-        for (int i = 0;i < n;i++) out.print(ans[i]+" ");
+        out.println(ans);
     }                             
     public static void main(String[] args) throws Exception {
         test = 1; //ni(in.readLine());
